a66f0f510e6229567b033c5693921b250b51199b
MaksymTymeichuk <105876357+MaksymTymeichuk@users.noreply.github.com>
2022-12-13 17:26:58 +0200

    some

--- script2.js
+++ script2.js
@@ -2,10 +2,16 @@ let letter_arr = ['Аа','Бб','Вв','Гг','Ґґ','Дд','Ее','Єє','Жж'
 arr = document.getElementsByClassName('text'),
 block_arr = document.getElementsByClassName("letter"),
 counter = 0,
-img_arr = ["agrus.jpg"]
-    
-function letter_info(letter){
-    console.log("yes")
+img_arr = ["agrus.jpg"],
+letter_info1 = document.getElementById("letter_info").addEventListener("click",letter_info(arr[0],img_arr[0]))
+for(let i =0;i>=arr.length;i++){
+    arr[i].style.innerText = letter_arr[0]
+}
+function letter_info(letter,img){
+    for(let i =0;i>=arr.length;i++){
+        block_arr[0].textContent = "s"
+    }
+    letter_info1.style.display = 'flex'
 }
     
 
